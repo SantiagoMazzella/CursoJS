d3b81c2329ad283d16ae2c927a841fd9c922d363
Santiago Mazzella <mazzellasantiago@gmail.com>
2022-08-30 10:14:05 -0300

    Se realizo el trabajo entregable para el día de la fecha

--- JS/app.js
+++ JS/app.js
@@ -1,26 +1,46 @@
+const remera = 1150;
+const jean = 2200;
+const campera = 7500;
 
-const hola = prompt(`¿Cuantos "Hola" desea?`);
+const cuotasRemera = remera / 12;
+const coutasJean = jean / 12;
+const cuotasCampera = campera / 12;
 
-for (let i = 0; i < hola; i++) {
-    alert("hola")
+let compra = Number(prompt(`Bienvenido, que desea comprar?
+1. Remera $1150
+2. Jean $2200
+3. Campera $7500
+4. Nada`));
+
+if (compra == 1) {
+    let pago = Number(prompt(`¿Como desea abonar su remera? 
+    1. Efectivo
+    2. 12 cuotas`));
+    if(pago == 1){
+        alert("Su compra fue realizada con exito")
+    } else if (pago == 2) {
+        alert(`Perfecto!, usted pagara 12 cuotas de ${cuotasRemera}`)
+    }   
+} else if (compra == 2) {
+    let pago = Number(prompt(`¿Como desea abonar su Jean? 
+    1. Efectivo
+    2. 12 cuotas`));
+    if(pago == 1){
+        alert("Su compra fue realizada con exito")
+    } else if (pago == 2) {
+        alert(`Perfecto!, usted pagara 12 cuotas de ${coutasJean}`)
+    }  
+} else if (compra == 3) {
+    let pago = Number(prompt(`¿Como desea abonar su Campera? 
+    1. Efectivo
+    2. 12 cuotas`));
+    if(pago == 1){
+        alert("Su compra fue realizada con exito")
+    } else if (pago == 2) {
+        alert(`Perfecto!, usted pagara 12 cuotas de ${cuotasCampera}`)
+    }  
+} else if (compra == 4) {
+    alert("Gracias, vuelva pronto");
+} else {
+    alert("Respuesta invalida, dijite el numero de la opccion porfavor");
 }
-
-const numero = Number(prompt("¿La tabla de que numero quiere ver?"));
-
-
-for (let i = 1; i <= 10; i++){
-    let resultado = i * numero;
-    alert(`${resultado}`)
-}
-
-
-let entrada = prompt("Ingrese un texto o ESC para interrumpir");
-
-let texto = " "
-
-while (entrada != "ESC" && entrada != "esc"){
-    texto += entrada + " ";
-    entrada = prompt("Ingrese un texto o ESC para interrumpir");
-}
-
-alert(texto);
