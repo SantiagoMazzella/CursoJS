3fd7aaf2d650d0da9e8ee1ebda3961fec12df712
Santiago Mazzella <mazzellasantiago@gmail.com>
2022-09-08 19:04:21 -0300

    Primera preentrega del proyecto final

--- JS/app.js
+++ JS/app.js
@@ -1,46 +1,48 @@
-const remera = 1150;
-const jean = 2200;
-const campera = 7500;
+class Productos {
+    constructor(articulo, precio){
+        this.articulo = articulo
+        this.precio = precio
+    }
 
-const cuotasRemera = remera / 12;
-const coutasJean = jean / 12;
-const cuotasCampera = campera / 12;
-
-let compra = Number(prompt(`Bienvenido, que desea comprar?
-1. Remera $1150
-2. Jean $2200
-3. Campera $7500
-4. Nada`));
-
-if (compra == 1) {
-    let pago = Number(prompt(`¿Como desea abonar su remera? 
-    1. Efectivo
-    2. 12 cuotas`));
-    if(pago == 1){
-        alert("Su compra fue realizada con exito")
-    } else if (pago == 2) {
-        alert(`Perfecto!, usted pagara 12 cuotas de ${cuotasRemera}`)
-    }   
-} else if (compra == 2) {
-    let pago = Number(prompt(`¿Como desea abonar su Jean? 
-    1. Efectivo
-    2. 12 cuotas`));
-    if(pago == 1){
-        alert("Su compra fue realizada con exito")
-    } else if (pago == 2) {
-        alert(`Perfecto!, usted pagara 12 cuotas de ${coutasJean}`)
-    }  
-} else if (compra == 3) {
-    let pago = Number(prompt(`¿Como desea abonar su Campera? 
-    1. Efectivo
-    2. 12 cuotas`));
-    if(pago == 1){
-        alert("Su compra fue realizada con exito")
-    } else if (pago == 2) {
-        alert(`Perfecto!, usted pagara 12 cuotas de ${cuotasCampera}`)
-    }  
-} else if (compra == 4) {
-    alert("Gracias, vuelva pronto");
-} else {
-    alert("Respuesta invalida, dijite el numero de la opccion porfavor");
 }
+
+const carrito = []
+
+function agregarProducto (carrito) {
+    const articulo = prompt(`¿Que articulo desea comprar?`);
+    const precio = Number(prompt(`¿De que valor?`));
+
+    const nuevoProducto = new Productos (articulo, precio);
+
+    carrito.push(nuevoProducto);
+}
+
+function mostrarCarrito () {
+    carrito.forEach(producto => {
+        console.log(`${producto.articulo}: ${producto.precio}`)
+    });
+}
+
+
+
+ function totalCarrito () {
+    const total = carrito.reduce((acc, el) => acc + el.precio ,0)
+
+    alert(`Gracias por su compra, su total es de ${total}`)
+ }
+
+ let opcion = prompt("Que desea realizar? \n 1. Elegir prenda \n 2. Finalizar compra \n 3. ver carrito (f12) \n 4. finalizar");
+
+ while(opcion !== "4" ) {
+    if (opcion == "1") {
+        agregarProducto(carrito);
+        alert("Prenda agregada al carrito!")
+    }
+    if (opcion == "2") {
+        totalCarrito(carrito);
+    }
+    if (opcion == "3") {
+        mostrarCarrito(carrito);
+    }
+    opcion = prompt("Que desea realizar? \n 1. Elegir prenda \n 2. Finalizar compra \n 3. ver carrito (f12) \n 4. finalizar")
+ }
